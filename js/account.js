cdb18fbef0aa0be0382fa8d13caa9dfb30c99314
谭翔 <510754787@qq.com>
2017-07-17 19:35:03 +0800

    first

---
+++ js/account.js
@@ -0,0 +1,70 @@
+$(function() {
+	
+	// 兼容火狐、IE8   
+	// 显示遮罩层的函数
+	function showMask(e) {
+		var winH = $(window).height(),
+			winW = $(window).width();
+		e.css("height", winH);
+		e.css("width", winW);
+		$('body').css({
+			height: winH,
+			width: winW
+			//overflow: 'hidden' 
+		});
+		e.show();
+	}
+	
+	// 隐藏遮罩层的函数
+	function hideMask(e) {
+		e.hide();
+	}
+	
+	// 点击显示遮罩层
+	$('.change-pw').on('click', function() {
+		var obj = $('.mask.safe');
+		showMask(obj);
+		$('.sure.safe').slideDown();
+	});
+	
+	// 点击隐藏遮罩层
+	$('.close, .msg-button').on('click', function(){
+		var obj = $('.mask.safe');
+		hideMask(obj);
+	});
+		
+	// 确认修改密码时的弹框
+	$('.sub-sure').on('click', function() {
+		var obj = $('.mask.sure-newpwd');
+		showMask(obj);
+		var sure = $('.sure.sure-newpwd');
+		sure.slideDown();
+		$('.close, button').on('click', function() {
+			sure.slideUp();
+			hideMask(obj);
+		})
+	});
+	
+})
+/**********************************************************************************************************/
+function getTabActiveH(obj){
+	return obj.outerHeight();
+}
+$(function(){
+	var tab = $('.sesstion-right .sesstion-tab');
+	var sesstionLeft = $('.sesstion-left');
+	var result = getTabActiveH($('.sesstion-right .sesstion-tab.active'));
+	console.log(sesstionLeft)
+	sesstionLeft.css({
+		height: result
+	});
+	$('.sesstion-left .op-item').on('click', function(){
+		var index = $(this).index() - 2;
+		$(this).addClass('active').siblings().removeClass('active')
+		tab.eq(index).addClass('active').siblings().removeClass('active');
+		result = getTabActiveH($('.sesstion-right .sesstion-tab.active'));
+		console.log(result)
+	})
+})
+
+/******************************************************************************************************/
