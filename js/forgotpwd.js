9d29e170233fca0bf3d0783762d92ba5cf9bc859
谭翔 <510754787@qq.com>
2017-07-19 17:29:30 +0800

    san

--- js/forgotpwd.js
+++ js/forgotpwd.js
@@ -2,7 +2,7 @@ $(function(){
 	// 找回密码页面的点击下一步
 	$('.first-next').on('click', function(){
 		// 设置顶部面包屑导航的背景
-		var obj = $('.return-pwd li');
+		var obj = $('.press-tips li');
 		obj.first().removeClass('active');
 		obj.last().addClass('active');
 		$('.return-pwd-item').eq(1).addClass('active').siblings().removeClass('active');
